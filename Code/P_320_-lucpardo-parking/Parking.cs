249b673b1e1de1c51ba39fcdceeb01d8e5aa4c74
Luca Pardo <pj85psp@eduvaud.ch>
2026-09-18 11:19:04 +0200

    Feat: ajout du système d'erreur de entrerVoiture et check de syntaxe

--- Code/P_320_-lucpardo-parking/Parking.cs
+++ Code/P_320_-lucpardo-parking/Parking.cs
@@ -10,6 +10,9 @@ namespace P_320__lucpardo_parking
     {
         string canton;
         int matricule;
+        const int PLACETOT = 20;
+        int placeOccupee = 0;
+        int placeLibre;
         public Parking(string canton, int matricule) 
         {
              this.canton = canton;
@@ -17,23 +20,29 @@ namespace P_320__lucpardo_parking
         }
         public void EntrerVoiture() 
         {
-            Console.Write("Veuillez entrer votre Canton ex: (Vaud = VD): ");
+            Console.WriteLine("Veuillez entrer votre Canton ex: (Vaud = VD): ");
             canton = Console.ReadLine();
-            if (canton.Length != 2) 
+            while (canton.Length != 2) 
             {
-                Console.Write("\nValeur invalide");
-                EntrerVoiture();
+                Console.WriteLine("\nValeur invalide");
+                Console.WriteLine("Veuillez entrer votre Canton ex: (Vaud = VD): ");
+                canton = Console.ReadLine();
             }
             Console.Write("Veuillez entrer votre matricule: ");
-            string matriculestring = matricule.ToString();
-            matriculestring = Console.ReadLine();
-            if (matriculestring.Length != 6 )
+            string matriculestring = Console.ReadLine();
+            bool matriculeInt = int.TryParse(matriculestring, out int nombre);
+            while (matriculestring.Length != 6 || matriculeInt == false)
             {
                 Console.WriteLine("Valeur invalide");
-                EntrerVoiture();
+                Console.Write("Veuillez entrer votre matricule: ");
+                matriculestring = Console.ReadLine();
+                matriculeInt = int.TryParse(matriculestring, out nombre);
             }
-            Console.Write($"{canton}-{matriculestring}");
-            
+            string plaque = $"{canton}-{matriculestring}";
+            Console.WriteLine($"Bonjour {plaque}, voici les places libres: ");
+            Console.Write($"Veuillez choisir votre place: ");
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Code/P_320_-lucpardo-parking/Voiture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_320__lucpardo_parking
+{
+    internal class Voiture
+    {
+        public Voiture() { }
+    }
+}
